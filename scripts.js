e651f817346c81bd384eec237c569e840d761e78
Maxime-Alexandre Gosselin <61528391+Notlein@users.noreply.github.com>
2023-04-25 19:25:31 -0400

    fix mardi

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 // Variables
-var json; 
+var json;
 var arraytofill;
 
 // CONSTANTS
 const prefix_thumb = "thumbnail/"; // prefixe pour la video thumb -> std
-const vids = "/videos/"
+const vids = "/videos/" //
 const csvUrl = './csv/listenoms.csv';
 const prefix = vids + prefix_thumb;
 
@@ -14,9 +14,18 @@ const prefix = vids + prefix_thumb;
  * - Backend-Frontend diagram (Figma ?)
  */
 
+function brasseListe(json){
+    for (i = json.length - 2; i > 0; i--) {
+        j = Math.floor(Math.random() * i);
+        k = json[i]
+        json[i] = json[j]
+        json[j] = k
+    }
+    return json;
+}
 
 // AJAX call on content load -> JSON - initialisation (vide)
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
     $.ajax({
         url: "4h.json",
         data: "data",
@@ -25,8 +34,8 @@ document.addEventListener("DOMContentLoaded", function(){
             json = response;
         }
     });
-    
-// AJAX call (fetch) -> csv(id,name,link,thumb)
+
+    // AJAX call (fetch) -> csv(id,name,link,thumb)
     fetch(csvUrl)
         .then(response => response.text())
         .then(data => {
@@ -37,18 +46,27 @@ document.addEventListener("DOMContentLoaded", function(){
                 const obj = {};
                 const currentLine = lines[i].split(',');
                 for (let j = 0; j < headers.length; j++) {
-                  obj[headers[j]] = currentLine[j];
+                    obj[headers[j]] = currentLine[j];
                 }
                 json.push(obj);
             }
+
+            brasseListe(json);
+            // i>1 puisque la ligne 0 ne doit pas être prise en compte
+            
+
         });
 });
+
 
 /**
  * Ajoute un élément div>video au contenu ".grille-video"
  * @param {int} id - L'index à ajouter
  */
 function ajouteGrilleDiv(id) {
+
+    
+
     const contenu = document.querySelector('.grille_video');
     let wrapper = document.createElement("div");
     let video = document.createElement("video");
@@ -59,7 +77,7 @@ function ajouteGrilleDiv(id) {
     wrapper.appendChild(video);
 
     wrapper.name = json[id]['name'];
-    wrapper.id = "vid-" +(id + 1);
+    wrapper.id = "vid-" + (id + 1);
     wrapper.classList.add("div_video");
 
     video.src = prefix + json[id]['thumb'];
@@ -67,14 +85,17 @@ function ajouteGrilleDiv(id) {
     video.loop = true;
     video.muted = true;
     //video.src = link;
-    
-    wrapper.addEventListener('mouseenter', function () {video.play()});
-    wrapper.addEventListener('mouseleave', function () {video.pause()});
+
+    wrapper.addEventListener('mouseenter', function () {
+        video.play()
+    });
+    wrapper.addEventListener('mouseleave', function () {
+        video.pause()
+    });
+
 
     // Dette technique - > nouveau system de player full screen prev play next + link a ajouter
 }
-
-
 
 
 
@@ -83,8 +104,14 @@ function ajouteGrilleDiv(id) {
 window.addEventListener('scroll', () => {
     const scrollMax = document.documentElement.scrollHeight - window.innerHeight;
     const scrolled = window.scrollY;
-    if ((Math.ceil(scrolled) >= (scrollMax - 10))){
-            // ajouteGrilleDiv(i);
-            // pour ajouter les cases individuellement
+    
+
+    if ((Math.ceil(scrolled) >= (scrollMax - 10))) {
+        let x = json.length -1;
+        for(let i = 0; i < x; i++)
+            ajouteGrilleDiv(i);
+
+        
+        // pour ajouter les cases individuellement
     }
 });
