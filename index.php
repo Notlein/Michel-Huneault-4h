43bc0015d10667df894db580a2b552880fdff9ed
Moïse Kaseka <mokadev21@gmail.com>
2023-05-17 13:28:59 -0400

    quelque changement dans css

--- index.php
+++ index.php
@@ -14,8 +14,6 @@
 			href="https://cdnjs.cloudflare.com/ajax/libs/video.js/7.10.2/video-js.min.css"
 			rel="stylesheet"
 		/>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="path/to/fontawesome.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/video.js/7.10.2/video.min.js"></script>
   <!-- Google tag (gtag.js) --> 
