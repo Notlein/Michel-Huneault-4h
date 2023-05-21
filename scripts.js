8bf11727ad9943f3b1bce1ac5809fe170054f277
Moïse Kaseka <mokadev21@gmail.com>
2023-05-21 19:48:29 -0400

    logo, next wip, langue default, mobile wip

--- scripts.js
+++ scripts.js
@@ -14,9 +14,10 @@ var btnFr = document.querySelector('.fr p');
 var btnEn = document.querySelector('.en p');
 var btnEu = document.querySelector('.eu p');
 var btnEs = document.querySelector('.es p');
-var btnLangues = document.querySelector('.btn_langues p');
+var btnLangues = document.querySelector('.btn_langues div');
 var btnApropos = document.querySelector('.apropos p');
 var titre4h = document.querySelector('.titre_4h');
+var titre4hHEAD = document.querySelector('title');
 var lg = 'fr';
 var languages;
 
@@ -52,9 +53,10 @@ onload = (event) => {
     btnEn.innerHTML = languages[lg]['btn_en'];
     btnEu.innerHTML = languages[lg]['btn_eu'];
     btnEs.innerHTML = languages[lg]['btn_es'];
-    btnLangues.innerHTML = languages[lg]['btn_langues'];
+    btnLangues.innerHTML = "<svg class='lg_logo' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d='M352 256c0 22.2-1.2 43.6-3.3 64H163.3c-2.2-20.4-3.3-41.8-3.3-64s1.2-43.6 3.3-64H348.7c2.2 20.4 3.3 41.8 3.3 64zm28.8-64H503.9c5.3 20.5 8.1 41.9 8.1 64s-2.8 43.5-8.1 64H380.8c2.1-20.6 3.2-42 3.2-64s-1.1-43.4-3.2-64zm112.6-32H376.7c-10-63.9-29.8-117.4-55.3-151.6c78.3 20.7 142 77.5 171.9 151.6zm-149.1 0H167.7c6.1-36.4 15.5-68.6 27-94.7c10.5-23.6 22.2-40.7 33.5-51.5C239.4 3.2 248.7 0 256 0s16.6 3.2 27.8 13.8c11.3 10.8 23 27.9 33.5 51.5c11.6 26 20.9 58.2 27 94.7zm-209 0H18.6C48.6 85.9 112.2 29.1 190.6 8.4C165.1 42.6 145.3 96.1 135.3 160zM8.1 192H131.2c-2.1 20.6-3.2 42-3.2 64s1.1 43.4 3.2 64H8.1C2.8 299.5 0 278.1 0 256s2.8-43.5 8.1-64zM194.7 446.6c-11.6-26-20.9-58.2-27-94.6H344.3c-6.1 36.4-15.5 68.6-27 94.6c-10.5 23.6-22.2 40.7-33.5 51.5C272.6 508.8 263.3 512 256 512s-16.6-3.2-27.8-13.8c-11.3-10.8-23-27.9-33.5-51.5zM135.3 352c10 63.9 29.8 117.4 55.3 151.6C112.2 482.9 48.6 426.1 18.6 352H135.3zm358.1 0c-30 74.1-93.6 130.9-171.9 151.6c25.5-34.2 45.2-87.7 55.3-151.6H493.4z'/></svg>";
     btnApropos.innerHTML = languages[lg]['apropos'];
     titre4h.innerHTML = languages[lg]['titre'];
+    titre4hHEAD.innerHTML = languages[lg]['titre_head'];
 
 };
 // AJAX call on content load
@@ -153,6 +155,7 @@ function ajouteGrilleDiv(id) {
     
     // Dette technique - > nouveau system de player full screen prev play next + link a ajouter
     wrapper.addEventListener("click", function addFs() {
+        document.querySelector(".grille_video").style.display = "none";
         let fs_contenu = document.querySelector('.div_contenu');
         fs_contenu.style.zIndex = 10;
         document.querySelector('section').style.overflow = "hidden";
@@ -165,9 +168,11 @@ function ajouteGrilleDiv(id) {
 
         source.src = "https://"
             +CLIENT_ID+".cloudflarestream.com/"+VIDEO_ID+"/manifest/video.m3u8";
-        source.type = "application/x-mpegURL"
+        source.type = "application/x-mpegURL";
         
-        
+
+
+     
 
         
         fsvideo.id = "vid-"+(id+1);
@@ -185,7 +190,6 @@ function ajouteGrilleDiv(id) {
 
         let fs_player = videojs(document.getElementById(fsvideo.id));
         fs_player.play();
-
         btnExit.innerHTML = "<svg class='xlogo' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 384 512'><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d='M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z'/></svg>";
         btnExit.style.zIndex = 6;
         btnExit.style.opacity = 0;
@@ -204,6 +208,12 @@ function ajouteGrilleDiv(id) {
             btnExit.style.opacity = 0;
         })
 
+        document.addEventListener("keyup", function (event) { 
+            if(event.key === 'Escape'){
+                location.reload();
+            }
+        })
+
         
 
         btnNext.innerHTML = "next";
@@ -211,9 +221,14 @@ function ajouteGrilleDiv(id) {
         btnNext.style.position = 'relative';
         btnNext.classList.add('next')
         btnNext.style.opacity = 0;
-        btnNext.addEventListener("click", function() {
-            nextVideo();
-        });
+        btnNext.addEventListener("click", nextVideo);
+        document.addEventListener("keyup", function (event) { 
+            if(event.key === 'ArrowRight'){
+                console.log('next');
+                nextVideo();
+            }
+        })
+
 
         btnNext.addEventListener("mouseenter", function() {
             btnExit.style.opacity = 1;
@@ -234,19 +249,24 @@ function ajouteGrilleDiv(id) {
             fsvideo.id = "vid-"+(id + 1);
             id++;
             addFs();
-            fs_player.pause();
+            fs_player.muted(true);
             fs_contenu.style.transition = '1s';
             fs_contenu.style.translate = '-50%';
             await sleep(1000);
             removePreviousVideo();
-
+            
             
         }
+
+
     
         function removePreviousVideo(){
             fs_contenu.removeChild(fs_contenu.children[0]);
             fs_contenu.style.transition = '0s';
             fs_contenu.style.translate = '0%';
+            fsvideo.id = "vid-"+(id+1);
+            console.log(fsvideo.id);
+            fs_player.play();
         }
 
 
@@ -368,7 +388,6 @@ function changeLanguage(lg){
     btnEn.innerHTML = languages[lg]['btn_en'];
     btnEu.innerHTML = languages[lg]['btn_eu'];
     btnEs.innerHTML = languages[lg]['btn_es'];
-    btnLangues.innerHTML = languages[lg]['btn_langues'];
     btnApropos.innerHTML = languages[lg]['apropos'];
     document.querySelectorAll('.contenu_ap p').innerHTML = languages[lg]['p'];
     document.querySelectorAll('.contenu_ap h1').innerHTML = languages[lg]['titre'];
