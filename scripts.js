85ba13f6cc7845f3d718f891ca18f5f14c0b669d
Maxime-Alexandre Gosselin <61528391+Notlein@users.noreply.github.com>
2023-05-21 23:49:53 -0400

    corrections de quelques bogues. bad media query

--- scripts.js
+++ scripts.js
@@ -27,6 +27,17 @@ function brasseListe(vids) {
 }
 
 
+function changeLanguage(lg){
+    titre4h.innerHTML = languages[lg]['titre'];
+    btnFr.innerHTML = languages[lg]['btn_fr'];
+    btnEn.innerHTML = languages[lg]['btn_en'];
+    btnEu.innerHTML = languages[lg]['btn_eu'];
+    btnEs.innerHTML = languages[lg]['btn_es'];
+    btnApropos.innerHTML = languages[lg]['apropos'];
+    document.querySelectorAll('.contenu_ap p').innerHTML = languages[lg]['p'];
+    document.querySelectorAll('.contenu_ap h1').innerHTML = languages[lg]['titre'];
+}
+
 
 function iterateurGrille() { 
     for (let index = 0; index < LIMITE; index++) {
@@ -52,6 +63,16 @@ async function loadInit () {
         }
     });
 
+    await $.ajax({
+        url: "ecrit_apropos/languages.json",
+        data: "data",
+        dataType: "json",
+        success: function (response) {
+            languages = response;
+            console.log(languages);
+            changeLanguage('fr');
+        }
+    });
 
     // APPEL API Cloudflare
     const options = {
@@ -79,9 +100,21 @@ async function loadInit () {
         .catch(err => console.error(err));
         loaded = true;
         iterateurGrille();
-        
-    }
 
+        //probleme on call here
+
+    }
+    onload = (event) => {
+        btnFr.innerHTML = languages[lg]['btn_fr'];
+        btnEn.innerHTML = languages[lg]['btn_en'];
+        btnEu.innerHTML = languages[lg]['btn_eu'];
+        btnEs.innerHTML = languages[lg]['btn_es'];
+        btnLangues.innerHTML = "<svg class='lg_logo' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d='M352 256c0 22.2-1.2 43.6-3.3 64H163.3c-2.2-20.4-3.3-41.8-3.3-64s1.2-43.6 3.3-64H348.7c2.2 20.4 3.3 41.8 3.3 64zm28.8-64H503.9c5.3 20.5 8.1 41.9 8.1 64s-2.8 43.5-8.1 64H380.8c2.1-20.6 3.2-42 3.2-64s-1.1-43.4-3.2-64zm112.6-32H376.7c-10-63.9-29.8-117.4-55.3-151.6c78.3 20.7 142 77.5 171.9 151.6zm-149.1 0H167.7c6.1-36.4 15.5-68.6 27-94.7c10.5-23.6 22.2-40.7 33.5-51.5C239.4 3.2 248.7 0 256 0s16.6 3.2 27.8 13.8c11.3 10.8 23 27.9 33.5 51.5c11.6 26 20.9 58.2 27 94.7zm-209 0H18.6C48.6 85.9 112.2 29.1 190.6 8.4C165.1 42.6 145.3 96.1 135.3 160zM8.1 192H131.2c-2.1 20.6-3.2 42-3.2 64s1.1 43.4 3.2 64H8.1C2.8 299.5 0 278.1 0 256s2.8-43.5 8.1-64zM194.7 446.6c-11.6-26-20.9-58.2-27-94.6H344.3c-6.1 36.4-15.5 68.6-27 94.6c-10.5 23.6-22.2 40.7-33.5 51.5C272.6 508.8 263.3 512 256 512s-16.6-3.2-27.8-13.8c-11.3-10.8-23-27.9-33.5-51.5zM135.3 352c10 63.9 29.8 117.4 55.3 151.6C112.2 482.9 48.6 426.1 18.6 352H135.3zm358.1 0c-30 74.1-93.6 130.9-171.9 151.6c25.5-34.2 45.2-87.7 55.3-151.6H493.4z'/></svg>";
+        btnApropos.innerHTML = languages[lg]['apropos'];
+        titre4h.innerHTML = languages[lg]['titre'];
+        titre4hHEAD.innerHTML = languages[lg]['titre_head'];
+    
+    };
 
 /**
  * Ajoute un élément div>video au contenu ".grille-video"
@@ -168,14 +201,40 @@ function ajouteGrilleDiv(id) {
             //counter = 0; 
         });
 
-        
+        btnExit.addEventListener('mouseenter', function () {
+            btnExit.style.opacity = 1;
+            btnNext.style.opacity = 1;
+        })
 
+        btnExit.addEventListener('mouseleave', function () {
+            btnExit.style.opacity = 0;
+        })
+
+        document.addEventListener("keydown", function (event) { 
+            if(event.key === 'Escape'){
+                location.reload();
+            }
+        })
+
+        
+// verifier le bouton ici
         btnNext.innerHTML = "next";
         btnNext.style.zIndex = 4;
         btnNext.style.position = 'relative';
         btnNext.classList.add('next')
-        btnNext.addEventListener("click", function() {
-            nextVideo();
+        btnNext.style.opacity = 0;
+        btnNext.addEventListener("click", nextVideo);
+        document.addEventListener("keydown", function (event) { 
+            if(event.key === 'ArrowRight'){
+                console.log('next');
+                nextVideo();
+            }
+        })
+
+
+        btnNext.addEventListener("mouseenter", function() {
+            btnExit.style.opacity = 1;
+            btnNext.style.opacity = 1;
         });
 
         fs_player.on('ended', function() { 
@@ -273,6 +332,38 @@ btn_Apropos.addEventListener('click', function () {
         //counter = 0; 
     });
 })
+
+/**
+ * Ajoute un listener au bouton de langues
+ */
+
+document.querySelector('.fr').addEventListener('click', function () { 
+    console.log('fr');
+    lg = 'fr';
+    changeLanguage(lg);
+})
+
+document.querySelector('.en').addEventListener('click', function () { 
+    console.log('en');
+    lg = 'en';
+    changeLanguage(lg);
+})
+
+document.querySelector('.eu').addEventListener('click', function () { 
+    console.log('eu');
+    lg = 'eu';
+    changeLanguage(lg);
+})
+
+document.querySelector('.es').addEventListener('click', function () { 
+    console.log('es');
+    lg = 'es';
+    changeLanguage(lg);
+})
+
+/**
+ * Change la langue du site
+ */ 
 
 // let y = n
 // ==> n=1 -> [0,23]
